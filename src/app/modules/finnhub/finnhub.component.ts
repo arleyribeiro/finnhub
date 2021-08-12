daa9810c43676fe2913b27dbf26cfe52758dd54e
arleyribeiro <arley.sribeiro@gmail.com>
2021-08-12 13:42:16 -0300

    Added polling by stock

--- src/app/modules/finnhub/finnhub.component.ts
+++ src/app/modules/finnhub/finnhub.component.ts
@@ -12,7 +12,11 @@ export class FinnhubComponent implements OnInit, OnDestroy {
   public data: any = [];
   public urlControl = new FormControl(null, [Validators.required]);
   public _setData: any = {};
-  public pollingTime = 15000;
+  private _settings = {
+    changePercentageThreshold: 0.03,
+    highFrequencyPollingInterval: 2000,
+    lowFrequencyPollingInterval: 15000,
+  };
 
   private FINNHUB_DATA = 'finnhub-data';
   private _subscriptions = new Subscription();
@@ -25,7 +29,7 @@ export class FinnhubComponent implements OnInit, OnDestroy {
   ngOnInit(): void {
     const cache = sessionStorage.getItem(this.FINNHUB_DATA);
     this.data = cache ? JSON.parse(cache) : [];
-    this.updateQuotes();
+    this._restartPollingStocks();
   }
 
   public resetAll(): void {
@@ -34,59 +38,76 @@ export class FinnhubComponent implements OnInit, OnDestroy {
     sessionStorage.removeItem(this.FINNHUB_DATA);
   }
 
-  public updateQuotes() {
-    console.log(this.pollingTime++);
-    if (this.data) {
-      this.data.forEach((quote: any) => {
-        this.getDataFromUrl(quote.url);
-      });
-    }
-    this.polling();
-  }
-
-  public polling() {
-    setTimeout(this.updateQuotes.bind(this), this.pollingTime);
+  public pollingByStock(stock: any) {
+    setTimeout(
+      this._getDataFromUrl.bind(this, stock.url),
+      stock.settings.interval
+    );
   }
 
   public searchUrl() {
     const url = this.urlControl.value;
-    if (url) this.getDataFromUrl(url);
+    if (url) this._getDataFromUrl(url);
     this.urlControl.reset();
   }
 
-  public getDataFromUrl(url: string): void {
-    const symbol = this.getSymboName(url);
+  private _getDataFromUrl(url: string): void {
+    const symbol = this._getSymboName(url);
     if (!symbol) return;
     this._subscriptions.add(
       this._finnHubService.getDataFromUrl(url).subscribe((response) => {
-        const value = {
-          url: url,
-          currentPrice: response.c,
-          change: response.d,
-          percentChange: response.dp,
-          highPriceDay: response.h,
-          lowPriceDay: response.l,
-          openPriceDay: response.o,
-          previousClosePrice: response.pc,
-          timestamp: response.t,
-          symbol: symbol,
-        };
+        const stock = this._getStock(url, response, symbol);
 
-        this._setData[symbol] = {
-          ...value,
+        const interval = this._getPollingInterval(
+          stock.currentPrice,
+          stock.openPriceDay
+        );
+
+        const _stock = {
+          ...stock,
           settings: {
-            changePercentageThreshold: 3000,
-            highFrequencyPollingInterval: 2000,
-            lowFrequencyPollingInterval: 15000,
+            interval,
           },
+          lastPull: new Date(),
         };
+        this._setData[symbol] = _stock;
         this.data = Object.values(this._setData);
         sessionStorage.setItem(this.FINNHUB_DATA, JSON.stringify(this.data));
+        this.pollingByStock(_stock);
       })
     );
   }
 
-  private getSymboName(url: string): string {
+  private _restartPollingStocks() {
+    (this.data || []).forEach((stock: any) => {
+      this.pollingByStock(stock);
+    });
+  }
+
+  private _getStock(url: string, response: any, symbol: string) {
+    return {
+      url: url,
+      currentPrice: response.c,
+      change: response.d,
+      percentChange: response.dp,
+      highPriceDay: response.h,
+      lowPriceDay: response.l,
+      openPriceDay: response.o,
+      previousClosePrice: response.pc,
+      timestamp: response.t,
+      symbol: symbol,
+    };
+  }
+
+  private _getPollingInterval(currentPrice: number, openPriceDay: number) {
+    const percentageChange = Math.abs(openPriceDay - currentPrice) / 100.0;
+    return Math.abs(percentageChange) >=
+      this._settings.changePercentageThreshold
+      ? this._settings.highFrequencyPollingInterval
+      : this._settings.lowFrequencyPollingInterval;
+  }
+
+  private _getSymboName(url: string): string {
     if (!url) return '';
     const [http, params] = url.split('symbol=');
     const [symbol, token] = (params || '').split('&token=');
